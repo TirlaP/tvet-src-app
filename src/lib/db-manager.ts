ac77c6ebb2e1836b3c44178d6ffcbe122e618983
Example User <example@example.com>
2025-04-04 23:30:20 +0300

    Add fixes

--- src/lib/db-manager.ts
+++ src/lib/db-manager.ts
@@ -6,21 +6,13 @@ import {
   nominationProcessService as dexieNominationProcessService
 } from './services';
 
-import {
-  studentService as sqliteStudentService,
-  nominationService as sqliteNominationService,
-  supporterService as sqliteSupporterService,
-  adminService as sqliteAdminService,
-  nominationProcessService as sqliteNominationProcessService
-} from './sqlite-services';
-
 import { DB_CONFIG } from '../config';
 
 // Get database engine from configuration
 const DB_ENGINE = DB_CONFIG.ENGINE;
 
-export const studentService = DB_ENGINE === 'dexie' ? dexieStudentService : sqliteStudentService;
-export const nominationService = DB_ENGINE === 'dexie' ? dexieNominationService : sqliteNominationService;
-export const supporterService = DB_ENGINE === 'dexie' ? dexieSupporterService : sqliteSupporterService;
-export const adminService = DB_ENGINE === 'dexie' ? dexieAdminService : sqliteAdminService;
-export const nominationProcessService = DB_ENGINE === 'dexie' ? dexieNominationProcessService : sqliteNominationProcessService;
+export const studentService = dexieStudentService
+export const nominationService = dexieNominationService
+export const supporterService = dexieSupporterService
+export const adminService = dexieAdminService
+export const nominationProcessService = dexieNominationProcessService
